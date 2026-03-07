75e49460d7a55c49b891940d257b73a7234325cd
Ratan Jana <janaratan460@gmail.com>
2026-03-07 19:32:48 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,16 @@
+const accountId = 12345
+let accountEmail = "janaratan460@gmail.com"
+var password = 1234
+accountCity = "jaipur" /* */
+
+accountEmail="ratajnsnjn"
+password= 1234
+accountCity = "kolkata"
+console.log(accountId);
+
+
+/*
+prefer not to use var
+because of issue in block scope(curly braces) and functional scope
+*/
+console.table([accountEmail,password,accountCity])
