7b2d082a146efaab5ac45c3f56b146475dee8ac3
Ratan Jana <janaratan460@gmail.com>
2026-06-10 21:36:05 +0530

    10thjune2026

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -37,3 +37,40 @@ console.log(obj4)       //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
 const obj5 = Object.assign({}, obj1,obj2)
 console.log(obj4)       //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }  same result
 
+const user = [
+    {
+        id:"ratan123",
+        email:"ratanjana7600@gmail.com"
+    },
+    {
+        id:"ratan123",
+        email:"ratanjana7600@gmail.com"
+    },
+    {
+        id:"ratan123",
+        email:"ratanjana7600@gmail.com"
+    },
+    {
+        id:"ratan123",
+        email:"ratanjana7600@gmail.com"
+    }
+]
+
+user[1].email
+
+console.log(Object.keys(localstockuser))    //[ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.values(localstockuser))  //[ '123abc', 'Ratan', true ]
+console.log(Object.entries(localstockuser)) //[ [ 'id', '123abc' ], [ 'name', 'Ratan' ], [ 'isLoggedIn', true ] ]
+
+console.log(localstockuser.hasOwnProperty('isLogged'))  //false
+
+const course = {
+    website : "localstock",
+    users : 700,
+    developer : "Ratan"
+}
+
+const {developer} = course
+console.log(developer)      //Ratan
+const {developer : dev} = course
+console.log(dev)        //Ratan
