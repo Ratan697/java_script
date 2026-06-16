6a7224739451915ecb100919828660ee216c5a92
Ratan Jana <janaratan460@gmail.com>
2026-06-16 19:56:33 +0530

    16thjune2026

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -1,53 +1,53 @@
-// // ----Function Defination----
-// function sayMyName() {
-//     console.log("Ratan");
-// }
+// ----Function Defination----
+function sayMyName() {
+    console.log("Ratan");
+}
 
-// sayMyName    //Function referance
-// sayMyName()     //Ratan  (function call)
+sayMyName    //Function referance
+sayMyName()     //Ratan  (function call)
 
-// function addTwoNumber(num1,num2){       //num1 and num2 are the parameters
-//     console.log(num1 + num2);
-// }
+function addTwoNumber(num1,num2){       //num1 and num2 are the parameters
+    console.log(num1 + num2);
+}
 
-// addTwoNumber()      //NaN (didnot pass any argument inside the funtion call)
-// addTwoNumber(1,0)   //1
+addTwoNumber()      //NaN (didnot pass any argument inside the funtion call)
+addTwoNumber(1,0)   //1
 
-// addTwoNumber(2, "1")    //21
+addTwoNumber(2, "1")    //21
 
-// addTwoNumber(2 ,"r")    //2r
+addTwoNumber(2 ,"r")    //2r
 
-// const result = addTwoNumber(1,2);
-// console.log(result)     //3 & undefined (because console.log() prints a value, but it does not return it.)
+const result = addTwoNumber(1,2);
+console.log(result)     //3 & undefined (because console.log() prints a value, but it does not return it.)
 
-// function addTwoNumber2(num1,num2){
-//     let result2 = num1 + num2
-//     return result2
-// }
+function addTwoNumber2(num1,num2){
+    let result2 = num1 + num2
+    return result2
+}
 
-// let result3 = addTwoNumber2(1,1)
-// console.log(result3)
+let result3 = addTwoNumber2(1,1)
+console.log(result3)
 
 
 
-// function loginUserMessege(username){
-//         return `${username} just logged in`
-// }
+function loginUserMessege(username){
+        return `${username} just logged in`
+}
 
-// loginUserMessege("Ratan")       //does not do anything because i didnot print it
-// console.log(loginUserMessege("Ratan"))      //Ratan just logged in
+loginUserMessege("Ratan")       //does not do anything because i didnot print it
+console.log(loginUserMessege("Ratan"))      //Ratan just logged in
 
-// console.log(loginUserMessege())     //undefined just logged in
+console.log(loginUserMessege())     //undefined just logged in
 
-// function loginUserMessege2(username2){
-//         if(username2 === undefined){        //username2 === undefined is same as !username2
-//             console.log("Enter a valid name")
-//         }
-//         else
-//         return `${username2} is just logged in`
-// }
+function loginUserMessege2(username2){
+        if(username2 === undefined){        //username2 === undefined is same as !username2
+            console.log("Enter a valid name")
+        }
+        else
+        return `${username2} is just logged in`
+}
 
-// console.log(loginUserMessege2())        //Enter a valid name
+console.log(loginUserMessege2())        //Enter a valid name
 
 
 
@@ -57,33 +57,33 @@
 
 // ---------------## FUnction part 2 ##---------------
 
-// function calculateCartPrice(num1){      //here we pass only one defined parameter
-//         return num1
-// }
+function calculateCartPrice(num1){      //here we pass only one defined parameter
+        return num1
+}
 
-// console.log(calculateCartPrice(100))        //100
+console.log(calculateCartPrice(100))        //100
 
-// // ... is a rest operator and spread operator 
+// ... is a rest operator and spread operator 
 
-// function calculateCartPrice2(...num1){
-//     return num1
-// }
+function calculateCartPrice2(...num1){
+    return num1
+}
 
-// console.log(calculateCartPrice2(100,200,300,400))       //[ 100, 200, 300, 400 ]  all the numbers are stored in an array by usng this rest operator(...)
-
-
-
-// const user = {
-//     username : "Ratan",
-//     price : 99
-// }
+console.log(calculateCartPrice2(100,200,300,400))       //[ 100, 200, 300, 400 ]  all the numbers are stored in an array by usng this rest operator(...)
 
 
-// function handleObject(anyobject){
-//         console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
-// }
 
-// handleObject(user)      //[ 100, 200, 300, 400 ]
+const user = {
+    username : "Ratan",
+    price : 99
+}
+
+
+function handleObject(anyobject){
+        console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+handleObject(user)      //[ 100, 200, 300, 400 ]
 
 const myNewArray = [100,200,300,400]
 
@@ -91,4 +91,5 @@ function returnSecondValue(getArray){
         return getArray[1]
 }
 
-console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue(myNewArray))      //200
+
---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,78 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+// console.log(a)      //10
+// console.log(b)      //20
+// console.log(c)      //30
+
+
+
+// {} is scope in case of function and loops etc.
+
+// if (true) {             //block scope
+//     let a = 10
+//     const b = 20
+//     var c = 30
+// }
+
+// console.log(a)      //a is not defined
+// console.log(b)      //console.log(b)
+// console.log(c)      //30
+
+
+
+// let a = 100
+
+// if(true){
+//     let a = 10
+//     console.log(`block scope ${a}`)     //block scope 10
+// }
+
+// console.log(`global scope ${a}`)        //global scope 100
+
+
+
+// -----NESTED SCOPE----
+
+// function one(){                         //global scope cannot use a local scope but local scope can use global scope
+//     const username = "Ratan"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username)
+//     }
+//     // console.log(website)     //error because i cannot access website from function two into function one
+
+//     two()
+// }
+
+// one()       //Ratan
+
+
+
+// if(true){
+//     const username = "Ratan"
+    
+//     if(username === "Ratan"){
+//         const website = " youtube"
+//         console.log(username + website)
+//     }
+
+//     // console.log(website)     //i cannot access website outside the if block scope
+// }
+
+// console.log(username)       //i cannot print it because the scope is inside the if block
+
+// ----------------  INTERESTING    -------------
+
+console.log(addone(1))
+function addone(num){
+        return num+1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num+2
+}
